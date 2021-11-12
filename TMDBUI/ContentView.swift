4e3e2f06d560a10e45e029095a69a6257b210fa6
vincent <vincent.lai.cn@gmail.com>
2021-11-11 19:41:39 -0800

    Initial Commit

---
+++ TMDBUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TMDBUI
+//
+//  Created by Qiang Lai on 2021-11-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TMDBUI/TMDBUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBUIApp.swift
+//  TMDBUI
+//
+//  Created by Qiang Lai on 2021-11-11.
+//
+
+import SwiftUI
+
+@main
+struct TMDBUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
